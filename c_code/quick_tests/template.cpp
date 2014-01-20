5730d54225d9d1a4c5fc52ccbd6071e0b96fb73f
Jack Smith <jack01110111@yahoo.co.uk>
2014-01-20 13:51:41 +0000

    adding a simple class

--- c_code/quick_tests/template.cpp
+++ c_code/quick_tests/template.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+
+using namespace std;
+
+class calc {
+    public:
+        int mult(int x, int y);
+        int add(int x, int y);
+};
+int calc::mult(int x, int y){
+    return x*y;
+}
+int calc::add(int x, int y){
+    return x+y;
+}
+
+
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    int x=6;
+    int y=3;
+
+    calc myCalc;
+    cout << "x=" << x << " y=" << y << endl << "x + y = " << myCalc.add(x,y) << endl << "x * y = " << myCalc.mult(x,y) << endl;
+
+
 
     return 0;
 }
