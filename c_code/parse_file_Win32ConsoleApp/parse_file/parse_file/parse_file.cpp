7b697f09a6381462f3885e1795fb12abb9f7759d
Stewart Fraser <stewart.fraser@f-e-t.com>
2016-11-09 12:59:22 +0000

    Initial commit of parse_file/ Win32 Console Application.

---
+++ c_code/parse_file_Win32ConsoleApp/parse_file/parse_file/parse_file.cpp
@@ -0,0 +1,16 @@
+// parse_file.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Test" << endl;
+
+	system("pause");
+	return 0;
+}
+
