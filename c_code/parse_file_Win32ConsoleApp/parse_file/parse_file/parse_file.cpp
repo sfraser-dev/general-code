3e24ed359f48c60c8d6a8abc447c98c5c80b0d16
Stewart Fraser <stewart.fraser@f-e-t.com>
2016-11-11 16:34:07 +0000

    Using better C++ style.

--- c_code/parse_file_Win32ConsoleApp/parse_file/parse_file/parse_file.cpp
+++ c_code/parse_file_Win32ConsoleApp/parse_file/parse_file/parse_file.cpp
@@ -7,58 +7,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <algorithm>
+#include <sstream>
 using namespace std;
 
-const int MAX_CHARS_PER_LINE = 2048;
-const int MAX_TOKENS_PER_LINE = 20;
-const char* const DELIMITER = ",";
-
-bool getNextLine() {
+bool readEncodingProfiles() {
 	ifstream fin;
 	fin.open("F:\\dev\\general\\c_code\\parse_file_Win32ConsoleApp\\parse_file\\parse_file\\VitecEncodingProfiles.dat");
 	
 	if (!fin.good()) 
 		return false; // exit if file not found
 
-	// read each line of the file
-	while (!fin.eof()){
-		// read an entire line into memory
-		char buf[MAX_CHARS_PER_LINE];
-		fin.getline(buf, MAX_CHARS_PER_LINE);
-    
-		// parse the line into blank-delimited tokens
-		int n = 0; // for-loop index
-		// array to store memory addresses of the tokens in buf
-		const char* token[MAX_TOKENS_PER_LINE] = {}; // initialize to 0
-    
-		// parse the line
-		token[0] = strtok(buf, DELIMITER); // first token
-		if (token[0]) { // zero if line is blank
-			for (n = 1; n < MAX_TOKENS_PER_LINE; n++){
-				token[n] = strtok(0, DELIMITER); // subsequent tokens
-				if (!token[n]) break; // no more tokens
-			}
-		}
-		else {
-			continue; // ignore blank lines
+	// read each line from the encoding-profiles file
+	string line;
+	while (getline (fin, line)){
+		// ignore empty lines
+		if (line.length() == 0){
+			continue;
 		}
 
-		// process (print) the tokens
-		for (int i = 0; i < n; i++){	// n = #of tokens
-			cout << "Token[" << i << "] = " << token[i] << endl;
-			string tokenStart = "";
-			// grab the first seven characters of the token
-			tokenStart += token[i][0];
-			tokenStart += token[i][1];
-			tokenStart += token[i][2];
-			tokenStart += token[i][3];
-			tokenStart += token[i][4];
-			tokenStart += token[i][5];
-			tokenStart += token[i][6];
-			// remove blank spaces
-			tokenStart.erase(remove_if(tokenStart.begin(), tokenStart.end(), isspace), tokenStart.end());
-			cout << "str: " << tokenStart << endl;
+		// get each token (delimited by commas) from each line
+		istringstream iss(line);
+		string token;
+		while (getline(iss, token, ',')){
+			// skip leading white space
+			iss >> ws;
+			cout << token << endl;
 		}
 		cout << endl;
 	}
@@ -69,10 +42,7 @@ bool getNextLine() {
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	bool result;
-
-	cout << "Test" << endl;
-	result = getNextLine();
+	bool result = readEncodingProfiles();
 
 	system("pause");
 	return 0;
