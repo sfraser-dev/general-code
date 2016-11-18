b73d3d647a394f7009e96629410f45682185cb76
Stewart Fraser <stewart.fraser@f-e-t.com>
2016-11-18 13:05:57 +0000

    Refactored to be more like its C# equivalent. More robust file checking of the VitecEncodingProfiles file.

--- c_code/parse_file_Win32ConsoleApp/parse_file/parse_file/parse_file.cpp
+++ c_code/parse_file_Win32ConsoleApp/parse_file/parse_file/parse_file.cpp
@@ -21,100 +21,80 @@ typedef struct _LWProfileDef {
 	CString audioEncoding;
 } LWProfileDef_t;
 
-// split line at delimiter
-void split(const std::string &s, char delim, std::vector<std::string> &elems) {
-    std::stringstream ss;
-    ss.str(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        elems.push_back(item);
-    }
-}
-
-// split line at delimiter
-std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-	split(s, delim, elems);
-    return elems;
-}
-
-// trim from start
-static inline std::string &ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
-    return s;
-}
-
-// trim from end
-static inline std::string &rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-    return s;
-}
-
-// trim from both ends
-static inline std::string &trim(std::string &s) {
-    return ltrim(rtrim(s));
-}
+// functions for splitting strings at a delimiter
+static void StringSplit(const std::string &s, char delim, std::vector<std::string> &elems);
+static std::vector<std::string> StringSplit(const std::string &s, char delim);
+// functions for trimming spaces from the front and the end of a string
+static inline std::string &StringTrimLeft(std::string &s);
+static inline std::string &StringTrimRight(std::string &s);
+static inline std::string &StringTrim(std::string &s);
 
 bool readEncodingProfiles() {
 
 	// open the encoding-profiles file
 	std::ifstream fin;
-	fin.open("F:\\dev\\general\\c_code\\parse_file_Win32ConsoleApp\\parse_file\\parse_file\\VitecEncodingProfiles.dat");
+	fin.open("C:\\ProgramData\\VisualSoft\\VisualWorks\\DVR\\QualityProfiles\\H264\\Hardware\\VitecEncodingProfiles.txt");
 	
 	// exit if file not found
 	if (!fin.good()) 
 		return false; 
 
 	// read the encoding-profiles file
-	std::string line;
-	std::vector<LWProfileDef_t> vecProfiles;
+	std::string lineFromFile;
+	std::vector<std::string> commentSplit;
+	std::string linesWithoutComments;
+	std::vector<std::string> noComments;
 	// grabbing each line from the encoding-profiles file
-	while (std::getline (fin, line)){
+	while (std::getline (fin, lineFromFile)){
 		// ignore empty lines
-		if (line.length() == 0)
+		if (lineFromFile.length() == 0)
 			continue;
 		// remove comments
-		std::vector<std::string> vectorCommentSplit = split(line, '#');
-		std::string lineNoComment = vectorCommentSplit[0];
+		commentSplit = StringSplit(lineFromFile, '#');
 		// ignore lines that are just comments
-		if (lineNoComment.length() == 0)
+		if (commentSplit[0].length() == 0)
 			continue;
+		// ignore lines that don't specifically start with "name"
+		if (StringTrim(commentSplit[0]).find("name") != 0)
+			continue;
+		noComments.push_back(commentSplit[0]);
+	}
 
-		// from each line, get the three tokens delimited by commas
-		std::vector<std::string> vectorCommaSplit = split(line, ',');
-		// expecting three comma separated strings (1) "name=...", (2) "video=..." and (3) "audio=..."
-		if (vectorCommaSplit.size() != 3)	
-			return false;
-		
-		// store tokens in a temporary ProfileDefinition structure
-		// convert std:strings to CStrings
-		// trim empty space from the start and end of the strings
+	std::vector<std::string> commaTokens;
+	std::string profileName;
+	std::vector<LWProfileDef_t> encodingProfiles;
+	for(int i=0; i<noComments.size(); i++){
+		// split each line into tokens (delimited by commas)
+		commaTokens = StringSplit(noComments[i],',');
+		if (commaTokens.size() != 3) {
+			// todo: handle error
+		}
+		// remove white space from start and end of each token and store them in a temporary structure
 		LWProfileDef_t tempProfile;
-		tempProfile.name = (trim(vectorCommaSplit[0])).c_str();
-		tempProfile.videoEncoding = (trim(vectorCommaSplit[1])).c_str();
-		tempProfile.audioEncoding = (trim(vectorCommaSplit[2])).c_str();
-		
-		// store the ProfileDefinition structures in a vector
-		vecProfiles.push_back(tempProfile);
+		tempProfile.name = (StringTrim(commaTokens[0])).c_str();
+		tempProfile.videoEncoding = (StringTrim(commaTokens[1])).c_str();
+		tempProfile.audioEncoding = (StringTrim(commaTokens[2])).c_str();
+		// store the temporary structure
+		encodingProfiles.push_back(tempProfile);
 	}
 	// close the encoding-profiles file
 	fin.close();
-	
-	// store the structures in an array (not a vector) to fit in easier with the current DVR code
-	LWProfileDef_t *arr7440ProfileDef = new LWProfileDef_t[vecProfiles.size()];
-	// test by printing to screen
-	for (int i=0; i<vecProfiles.size(); i++){
-		arr7440ProfileDef[i] = vecProfiles[i];
-		std::wcout << arr7440ProfileDef[i].name.GetString() << std::endl;
-		std::wcout << arr7440ProfileDef[i].videoEncoding.GetString() << std::endl;
-		std::wcout << arr7440ProfileDef[i].audioEncoding.GetString() << std::endl;
+
+	// copy the vector structs to the "old" array (minimise changes for now)
+	LWProfileDef_t *arr7440ProfileDef = new LWProfileDef_t[encodingProfiles.size()];
+	for (int i=0; i<encodingProfiles.size(); i++)
+		arr7440ProfileDef[i] = encodingProfiles[i];
+
+	// debug
+	for (int i=0; i<encodingProfiles.size(); i++){
+		std::wcout << encodingProfiles[i].name.GetString() << std::endl;
+		std::wcout << encodingProfiles[i].videoEncoding.GetString() << std::endl;
+		std::wcout << encodingProfiles[i].audioEncoding.GetString() << std::endl;
 		std::wcout << std::endl;
 	}
 
 	// tidy up
-	vecProfiles.clear();
+	encodingProfiles.clear();
 	delete [] arr7440ProfileDef;
 
 	return true;
@@ -126,4 +106,40 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	system("pause");
 	return 0;
+}
+
+// split string at delimiter
+void StringSplit(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+}
+
+// split string at delimiter
+std::vector<std::string> StringSplit(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+	StringSplit(s, delim, elems);
+    return elems;
+}
+
+// trim string from start
+static inline std::string &StringTrimLeft(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+// trim string from end
+static inline std::string &StringTrimRight(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
+}
+
+// trim string from both ends
+static inline std::string &StringTrim(std::string &s) {
+    return StringTrimLeft(StringTrimRight(s));
 }
